6ceecaefde410a399d863e7b71da9dcf391bed6b
KlossOne <j.senouni@gmail.com>
2017-01-22 11:19:32 +0100

    Verifications supplémentaires sur cetrtaines commandes
    Commande : touch et mkdir, verifient maitnenant si il existe un autre
    dossier/fichier ayant ce nom.
    Commande rename ajouté, permet de renommer un fichier ou un dossier.
    - fait attention a ce que le fichier source existe, et que le fichier de
    destination n'existe pas.
    
    -- 2017.1.18
    
    Mise en place de la commande pwd, permettant de savoir dans quel
    répertoire on se trouve. ( en mode absolu)

--- system_nondistribue/c/projet/bin/pwd.c
+++ system_nondistribue/c/projet/bin/pwd.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../constante.h"
 #include "../get_env.h"
 
@@ -8,9 +9,51 @@ int main(int argc, char ** argv){
 	unsigned long long parent;
 	get_parent(&parent);
 
-	if(parent == 0){
-		printf("/\n");
+	FILE *fp = fopen(FILE_INDEX, "r");
+        if(fp == NULL){
+        	printf("Fichier non trouver, ou acces non permis.\n");
+        }
+
+	int finish = 0;
+	char path[4096] = "";
+
+	char active;
+        unsigned long long tmp_parent;
+        char type_file;
+        char folder_name[255];
+	char concat[4096];
+
+	while(!finish){
+		
+				
+		fread(&active, sizeof(active), 1, fp);
+
+	
+		if(parent == 0){
+			concat[0] = '/'; concat[1] = '\0';
+			strcat(concat, path);
+			printf("%s\n", concat);
+			finish = 1;
+		} else {
+			fseek(fp, parent, SEEK_SET);
+
+			fread(&active, sizeof(active), 1, fp);
+                        fread(&tmp_parent, sizeof(tmp_parent), 1, fp);
+                        fread(&type_file, sizeof(type_file), 1, fp);
+                        fread(&folder_name, sizeof(folder_name), 1, fp);
+
+			strcpy(concat, folder_name);
+			strcat(concat, "/");
+		
+			strcat(concat, path);
+			strcpy(path, concat); 
+
+			
+			parent = tmp_parent;
+		}
 	}
+	
+	fclose(fp);
 
 	return EXIT_SUCCESS;
 }
