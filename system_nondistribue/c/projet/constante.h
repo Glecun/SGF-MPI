44e132e04f9b3420c5abfc8f7076752328e16cd0
KlossOne <j.senouni@gmail.com>
2017-01-23 22:15:08 +0100

    Commande Vim
    Permet d'ouvrir un fichier avec l'éditeur de texte vim.
    Création de la fonction put_file, qui enregistre les informatiosn dans
    le fichier stockage.jjg
    création de la fonction ajouterLigne ( que j'aurais du faire il y a bien
    longtemps..), qui permet d'ajouter un dossier ou un fichier dans
    index.jjg
    créations de quelque petite fonctions.

--- system_nondistribue/c/projet/constante.h
+++ system_nondistribue/c/projet/constante.h
@@ -1,5 +1,17 @@
 #define PATH_BIN_FILE "/home/joachim/system/c/projet/bin/" 
 #define FILE_INDEX "/home/joachim/system/c/projet/index.jjg"
+#define FILE_STOCK "/home/joachim/system/c/projet/stockage.jjg"
+#define PATH_FOLDER_SWAP "/home/joachim/system/c/projet/swp/"
 #define FILE_BASE_ENV 8+1+8+3*(1+8+255) // base du fichier index, comptant tout l'envirronement
 #define FICHIER 1
 #define DOSSIER 0
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _b : _a; })
--- system_nondistribue/c/projet/get_env.h
+++ system_nondistribue/c/projet/get_env.h
@@ -1,5 +1,9 @@
 void get_version(char * v);
-
+void get_index_last_cursor(unsigned long long * c);
+void get_stock_last_cursor(unsigned long long * c);
+void set_stock_last_cursor( unsigned long long cursor );
 void get_parent(unsigned long long * p); 
 void set_parent(unsigned long long p); 
 unsigned long long file_exist(char arg_type_file, char * arg_file_name);
+void put_file(char * path_filename, unsigned long long cursor_stock, unsigned long long * file_size);
+void ajouterLigne(unsigned long long cursor, char active, unsigned long long parent, char file_type, char * file_name, unsigned long long file_cursor_stock, unsigned long long file_size);
